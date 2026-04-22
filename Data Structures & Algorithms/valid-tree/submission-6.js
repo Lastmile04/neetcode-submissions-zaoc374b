186f216f1090b05e38c37dfec56a30f960906bbe
Dikshant Thakur <thakurdikshant05@gmail.com>
2026-04-22 10:07:33 +0530

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.js
@@ -0,0 +1,33 @@
+
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+    */
+    validTree(n, edges) {
+        if (edges.length > n - 1) return false;
+
+        const adj = Array.from({ length: n }, () => []);
+        const visit = new Set();
+
+        for (let [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        const dfs = (node, par) => {
+            if (visit.has(node)) return false;
+
+            visit.add(node);
+
+            for (const nei of adj[node]) {
+                if (nei == par) continue;
+                if (!dfs(nei, node)) return false;
+            }
+            return true;
+        }
+
+        return dfs(0,-1) && visit.size === n;
+    }
+}

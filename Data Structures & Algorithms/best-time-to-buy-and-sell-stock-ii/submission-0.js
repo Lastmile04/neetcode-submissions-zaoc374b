feee25ed02d2ed8a304fbcb81120e36992aeb908
Dikshant Thakur <thakurdikshant05@gmail.com>
2026-06-22 16:40:04 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        for(let i = 0; i< prices.length; i++){
+            if(i===0) continue;
+            const profit = prices[i]-prices[i-1];
+            maxProfit = profit > 0 ? maxProfit + profit : maxProfit + 0;
+        }
+        return maxProfit;
+    }
+}
